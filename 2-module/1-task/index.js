d5dc0859990f779772fd478fc749e585f1f0d432
Aleksey Balahnin <alesha961337@yandex.ru>
2022-03-19 11:02:07 +0300

    2 module 1 task done

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sumSalary = 0;
+    for (let key in salaries) {
+      if (salaries[key] >= 0 && isFinite(salaries[key])) {
+        sumSalary += salaries[key];
+      };
+    };
+  return sumSalary;
 }
