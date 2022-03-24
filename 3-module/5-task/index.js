23dfc138f1cf5da36f97165a8c833c244f9b4e19
Aleksey Balahnin <alesha961337@yandex.ru>
2022-03-24 12:09:47 +0300

    3 module 5 task done

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
-  // ваш код...
+  let arrStr = str.split(" ");
+  let arrFiltred = arrStr.filter(num => Number(num));
+  let mappingNum = arrFiltred.map(num => +num);
+  let sortingNum = mappingNum.sort((a, b) => a - b);
+  let objMinMax = {
+      min: sortingNum[0],
+      max: sortingNum[sortingNum.length - 1]
+  };
+  return objMinMax;
 }
