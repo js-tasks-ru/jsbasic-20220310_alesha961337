d34e2bf40a8568a7123293dcc14b1a8ee5e6e271
Aleksey Balahnin <alesha961337@yandex.ru>
2022-04-08 17:53:50 +0300

    6 module 1 task done

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,40 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.render();
   }
+
+  render() {
+    const elTable = document.createElement('TABLE');
+
+    const tableHead = `<thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>`;
+    
+    const rowsItem = this.rows.map(item => {
+      return `<tr>
+            <td>${item.name}</td>
+            <td>${item.age}</td>
+            <td>${item.salary}</td>
+            <td>${item.city}</td>
+            <td><button>X</button></td>
+        </tr>`;
+    }).join('');
+
+    const fullTable = tableHead + '<tbody>' + rowsItem + '</tbody>';
+    elTable.insertAdjacentHTML('beforeend', fullTable);
+
+    elTable.addEventListener('click', event => {
+      if (event.target.closest('button')) {
+        event.target.closest('tr').remove();
+      }
+    });
+    return elTable;
+  } 
 }
